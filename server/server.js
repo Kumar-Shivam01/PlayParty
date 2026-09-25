42e44abec708f8ce1ad733b60d10afef5638bc60
Kumar-Shivam01 <pkshivam9@gmail.com>
2026-09-25 17:16:36 +0530

    feat: add socketio server -install some dependencies

--- server/server.js
+++ server/server.js
@@ -1,13 +1,29 @@
 const express = require("express");
+const {Server} = require('socket.io');
+const http = require('http')
+const cors = require('cors')
 require('dotenv').config()
 
 const app = express();
 app.use(express.json())
+app.use(cors())
 
+const server = http.createServer(app)
 app.get('/',(req,res)=>{
     res.send('Hii this is PlayParty server running...')
 })
-
-app.listen(process.env.PORT,()=>{
+const io = new Server(server,{ //Socket.IO server with CORS configuration
+    cors:{ 
+        origin: "http://localhost:5173",
+        credentials: true
+    }
+})
+io.on('connection',(socket)=>{
+    console.log(`Client connected: ${socket.id}`)
+    socket.on('disconnect',()=>{
+        console.log(`Client disconnected: ${socket.id}`)
+    })
+})
+server.listen(process.env.PORT,()=>{
     console.log(`PlayParty server is running on http://localhost:${process.env.PORT}`)
 })
